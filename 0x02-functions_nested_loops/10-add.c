f2275a017de20507e0633c6bdb5542ba8a6eb902
root <3848@holberton.com>
2021-10-04 14:19:11 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,28 @@
+/*
+ * File: 10-add.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
+}
