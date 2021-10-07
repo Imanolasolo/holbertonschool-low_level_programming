47d13da72126cbea62b2fb62ab2241a67f1a01ab
root <3848@holberton.com>
2021-10-07 11:47:48 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+/* File: 2-mul.c
+ * Author: Imanol Asolo
+ */
+#include "main.h"
+
+/**
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+
+int mul;
+
+mul = a * b;
+
+return (mul);
+}
