ebbd99f4a92096f97f1c06928cc5d29620f9d5e5
root <3848@holberton.com>
2021-10-07 10:13:21 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/* File: 0-isupper.c
+ * Author: Imanol Asolo
+ */
+
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
