5843375439b521e232fa2600951f81833c2e8d09
root <3848@holberton.com>
2021-10-11 10:02:11 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/*
+ * File: 5-rev_string.c
+ * Author: Imanol Asolo
+ */
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int i = 0, j = 0;
+char str[500];
+
+while (*(s + i))
+{
+*(str + i) = *(s + i);
+i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+*(s + i) = *(str + j);
+j++;
+i--;
+}
+}
